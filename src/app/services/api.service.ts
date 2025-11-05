e3a9b671841469d2e9a19bcc32a952d77b99ff81
vikrant9528 <vikrantchaudhary258@gmail.com>
2025-11-05 17:58:35 +0530

    adding enviornment files

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { tap } from 'rxjs';
-
+import { environment } from '../../environments/environment'
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService implements OnInit {
   // private url = 'http://localhost:3000';
-  private url = 'http://192.168.1.161:3000';
+  // private url = 'http://192.168.1.161:3000';
 
   constructor(private http: HttpClient) { }
 
@@ -15,19 +15,19 @@ export class ApiService implements OnInit {
 
   }
   postLead(data: any) {
-    return this.http.post(this.url + '/leads', data);
+    return this.http.post(environment.apiUrl + '/leads', data);
   }
   getAllLeads(payload) {
-    return this.http.get(this.url + payload)
+    return this.http.get(environment.apiUrl + payload)
   }
   addUser(data: any) {
-    return this.http.post(this.url + '/users', data);
+    return this.http.post(environment.apiUrl + '/users', data);
   }
   getAllUsers(data: any) {
-    return this.http.get(this.url + '/users/' + data)
+    return this.http.get(environment.apiUrl + '/users/' + data)
   }
   signup(data: any) {
-    return this.http.post(this.url + '/auth/signup', data).pipe(
+    return this.http.post(environment.apiUrl + '/auth/signup', data).pipe(
       tap((res: any) => {
         if (res && res.token) {
           localStorage.setItem('token', res.token);
@@ -36,12 +36,12 @@ export class ApiService implements OnInit {
     );
   }
   login(data: any) {
-    return this.http.post(this.url + '/auth/login', data)
+    return this.http.post(environment.apiUrl + '/auth/login', data)
   }
   putApi(param, payload: any) {
-    return this.http.put(this.url + param, payload)
+    return this.http.put(environment.apiUrl + param, payload)
   }
   getApi(param: string) {
-    return this.http.get(this.url + param)
+    return this.http.get(environment.apiUrl + param)
   }
 }
